df187f8642ef76e424ddc7a03352907187423095
Maksym Kuhatov <mkuhatov@lohika.com>
2020-01-24 11:12:02 +0200

    fix: finished problem_1

---
+++ explanation_1.md
@@ -0,0 +1,10 @@
+## Square Root
+
+The solution is babylon method with `accuracy = 0.0001`
+
+### Time complexity
+The second parameter in the returned value shows amount of steps needed to solve it's problem. It grows slowly like 
+`O(long(n))`. 
+
+### Space complexity
+Amount of space needed to solve problem doesn't depends of `number` and it's constant `O(1)`
--- problem_1.py
+++ problem_1.py
@@ -1,5 +1,3 @@
-import math
-
 def sqrt(number):
     """
     Calculate the floored square root of a number
@@ -10,26 +8,23 @@ def sqrt(number):
        int: Floored Square Root
     """
 
-    if number <= 1:
-        return number
+    if number == 0 or number == 1:
+        return number, 0
 
-    def devide(acc):
-        # acc = round(acc)
-        # acc = math.floor(number)
-        pow2 = math.floor(acc * acc)
+    if number < 0:
+        return -1, 0
 
-        print(number, acc, pow2)
-        if pow2 == number:
-            return int(acc)
-        elif pow2 < number:
-            return devide((number + acc) / 2)
-        else:
-            return devide(acc / 2)
+    result = number / 2
+    accuracy = 0.0001
+    error = 1
+    steps = 0
 
-    print(number, " start")
-    result = devide(number / 2)
-    print(int(result))
-    return result
+    while error > accuracy:
+        steps += 1
+        result = (result + number / result) / 2
+        error = abs(number - result ** 2)
+
+    return int(result), steps
 
 
 def assert_equals(expected, actual):
@@ -37,10 +32,27 @@ def assert_equals(expected, actual):
 
 
 if __name__ == "__main__":
-    assert_equals(0, sqrt(0))
-    assert_equals(1, sqrt(1))
-    assert_equals(-9, sqrt(-9))
+    assert_equals((0, 0), sqrt(0))
+    assert_equals((1, 0), sqrt(1))
+    assert_equals((-1, 0), sqrt(-9))
 
-    assert_equals(3, sqrt(9))
-    assert_equals(4, sqrt(16))
-    assert_equals(5, sqrt(27))
+    assert_equals((1, 3), sqrt(2))
+    assert_equals((3, 3), sqrt(9))
+
+    assert_equals((4, 4), sqrt(16))
+
+    assert_equals((5, 5), sqrt(27))
+    assert_equals((6, 5), sqrt(36))
+    assert_equals((7, 5), sqrt(49))
+    assert_equals((8, 5), sqrt(64))
+
+    assert_equals((9, 6), sqrt(81))
+    assert_equals((10, 6), sqrt(100))
+    assert_equals((11, 6), sqrt(121))
+    assert_equals((12, 6), sqrt(144))
+    assert_equals((13, 6), sqrt(169))
+    assert_equals((14, 6), sqrt(196))
+    assert_equals((15, 6), sqrt(225))
+
+    assert_equals((16, 7), sqrt(256))
+    assert_equals((16, 7), sqrt(257))
