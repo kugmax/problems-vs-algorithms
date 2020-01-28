469c19cf664b82c980ce92d15d8e67175ee19f6e
Maxim Kugatov <maximkugatov@gmail.com>
2020-01-28 14:58:30 +0200

    feat: problem 6 is done

---
+++ explanation_6.md
@@ -0,0 +1,7 @@
+## Max and Min in a Unsorted Array
+
+### Time complexity
+`O(n)` needs to traverse all elements once
+
+### Space complexity
+`O(1)` there are only 2 references to store min, max results
---
+++ problem_6.py
@@ -0,0 +1,58 @@
+import sys
+
+
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    if not ints or len(ints) == 0:
+        return None, None
+
+    min = sys.maxsize
+    max = -sys.maxsize - 1
+
+    for i in ints:
+        if i > max:
+            max = i
+
+        if i < min:
+            min = i
+
+    return min, max
+
+
+def assert_equals(expected, actual):
+    assert (expected == actual), "expected {0}, actual {1}".format(expected, actual)
+
+
+if __name__ == "__main__":
+    assert_equals(
+        (None, None),
+        get_min_max([])
+    )
+
+    assert_equals(
+        (None, None),
+        get_min_max(None)
+    )
+
+    assert_equals(
+        (0, 9),
+        get_min_max([9, 3, 1, 2, 0, 5, 4, 6, 7, 8])
+    )
+
+    assert_equals(
+        (5, 5),
+        get_min_max([5, 5, 5, 5, 5])
+    )
+
+    assert_equals(
+        (0, 0),
+        get_min_max([0])
+    )
+
+
+
